3aa9e3b74e9ca91ee2bd2e3a3b2de7f81fc44674
michaelyousrie <michaelyousrie@gmail.com>
2025-04-17 13:26:50 +0400

    update href in the <x-card> component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -7,7 +7,7 @@
 
 <div href="{{ $link }}" class="border rounded-lg shadow-sm bg-gray-800 border-gray-700 hover:bg-gray-600 hover:cursor-pointer transition-colors duration-200">
     <div class="p-5">
-        <a href="#">
+        <a href="{{ $link }}">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{  $title  }}</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{  $desc }}</p>
